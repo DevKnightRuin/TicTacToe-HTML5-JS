1ea2f0602e037fbb86d5ddbe083c000b8e49c3cf
Nicholas Naquin <DevKnightRuin@outlook.com>
2022-11-10 11:48:14 -0600

    TicTacToe Initial Commit - Base project setup

---
+++ TicTacToe/api-template/js/main.js
@@ -0,0 +1,13 @@
+//Fetch API Outline
+// document.querySelector('button').addEventListener('click', getFetch)
+
+// function getFetch(){
+//   fetch(url)
+//       .then(res => res.json()) // parse response as JSON
+//       .then(data => {    
+//       })
+//       .catch(err => {
+//           console.log(`error ${err}`)
+//       });
+// }
+
