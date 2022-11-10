dfc6c1aabeffb192b043f5329859af344e6f7ce4
Nicholas Naquin <DevKnightRuin@outlook.com>
2022-11-10 13:31:00 -0600

    Added base notes for project

--- TicTacToe/js/main.js
+++ TicTacToe/js/main.js
@@ -11,3 +11,42 @@
 //       });
 // }
 
+
+
+
+
+
+// event listeners for each tile
+
+// gamestate - whos turn is currently active (use Math.rondom() to see who gets higher Number. 0-1)- active user gets image on board
+// decide who goes first
+
+// gameloop
+// PlayerTurn(){
+//     show display that it is the players move - set active game piece to be goku
+// }
+
+
+// cpuTurn(){
+//     disable event listeners to prevent player from doing anything while cpu takes their turn
+//     display gameboard turn
+//     set active game piece to vegeta
+//     add delay timer to mimick someone thinking
+//     decide how computer moves
+//     if tile  =   10- 
+//                  -10 
+//                  ---
+        
+// }
+            
+// checkForWinner()
+
+// p1Victory{
+
+// }
+
+// p2Victory{
+
+// }
+
+
