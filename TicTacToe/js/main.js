81576c84fee5ce702d9d28beb852182dbca99c4a
Nicholas Naquin <DevKnightRuin@outlook.com>
2022-11-16 20:51:39 -0600

    player/cpu turns work - but is buggy

--- TicTacToe/js/main.js
+++ TicTacToe/js/main.js
@@ -1,21 +1,30 @@
 //initialize game pieces
-const playerGamePiece = "img\goku.png";
-const cpuGamePiece = "img\vegeta.png"
+const playerGamePiece = "img/goku.png";
+const cpuGamePiece = "img/vegeta.png";
 // let isFirstMove = false;  //Variable only matters to cpu if making first move
 //Initialize event listeners with 1-9 top left = 1, left to right and top to bottom order
-const tile1 = document.querySelector('') = addEventListener('click', applyGamePiece)
-const tile2 = document.querySelector('') = addEventListener('click', applyGamePiece)
-const tile3 = document.querySelector('') = addEventListener('click', applyGamePiece)
-const tile4 = document.querySelector('') = addEventListener('click', applyGamePiece)
-const tile5 = document.querySelector('') = addEventListener('click', applyGamePiece)
-const tile6 = document.querySelector('') = addEventListener('click', applyGamePiece)
-const tile7 = document.querySelector('') = addEventListener('click', applyGamePiece)
-const tile8 = document.querySelector('') = addEventListener('click', applyGamePiece)
-const tile9 = document.querySelector('') = addEventListener('click', applyGamePiece)
+const tile1 = document.querySelector('#block_0');
+tile1.addEventListener('click', applyGamePiece);
+const tile2 = document.querySelector('#block_1')
+tile2.addEventListener('click', applyGamePiece)
+const tile3 = document.querySelector('#block_2')
+tile3.addEventListener('click', applyGamePiece)
+const tile4 = document.querySelector('#block_3')
+tile3.addEventListener('click', applyGamePiece)
+const tile5 = document.querySelector('#block_4')
+tile4.addEventListener('click', applyGamePiece)
+const tile6 = document.querySelector('#block_5')
+tile5.addEventListener('click', applyGamePiece)
+const tile7 = document.querySelector('#block_6')
+tile6.addEventListener('click', applyGamePiece)
+const tile8 = document.querySelector('#block_7')
+tile7.addEventListener('click', applyGamePiece)
+const tile9 = document.querySelector('#block_8')
+tile8.addEventListener('click', applyGamePiece)
 
 
 var activePlayer = ""; //Player ready to make a move
-var activeGamePiece; //The placemarker for the active player
+var activeGamePiece = ""; //The placemarker for the active player
 
 //Arrays of Rows and columns for fast vitory check - don't need to remake every time we check
 const arrRowTop = [ tile1, tile2, tile3]
@@ -43,8 +52,6 @@ const arrTiles = [
 const arrPlayableTiles = [ tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8, tile9 ]
 
 
-// decide who goes first
-
 //**********************  GAME LOOP  ********************* */
 whoGoesFirst();
 PlayerTurn(); 
@@ -59,85 +66,61 @@ cpuTurn();
             // *****Game flow functions *****
 
 function PlayerTurn(){
-
-//ACtivate player input
-    document.querySelector('gameBoard').classList.add('active');
-    document.querySelector('gameBoard').classList.remove('inactive');
-
-//show display that it is the players move - 
-    document.querySelector('p1Activity').innerHTML = "Player is thinking...";
-//set active game piece to be goku
-    activeGamePiece = playerGamePiece
+    //set active game piece
+    activeGamePiece = playerGamePiece;
 }
-
-
 function cpuTurn(){
-
-    //no check needed since cpu will place their piece at the end of this function call
-
-    //disable event listeners to prevent player from doing anything while cpu takes their turn
-     document.querySelector('gameBoard').classList.remove('active');
-     document.querySelector('gameBoard').classList.add('inactive');
-     
-    //set active game piece to vegeta
-     activeGamePiece = cpuGamePiece;
-     
-    //show display that it is the cpu's turn to mmove
-    document.querySelector('').innerHTML = "Cpu is thinking..."
-
-    //add delay timer to mimick someone thinking
-    setTimeout( await, () => { }, 2000)
-
-    cpuMove();    
-    PlayerTurn();
+    //set active game piece to vegeta if it is the cpu's turn.  JS scripts execute once on load, check to see if cpu is active to prevent issus
+    if(activePlayer == 'cpu'){
+        activeGamePiece = cpuGamePiece;
+        cpuRandomMove();    
+        checkForWinner();
+        activePlayer = 'player'
+        PlayerTurn();
+    }
 }
 //*****Game Action Functions*****
 
-function cpuMove(){  
-    cpuRandomMove();
-    //decide how computer moves
-    // if tile  =  1 0 - 
-    //             - 1 0 
-    //             - - -
-    //If player1 has 2 elements and the last one is empty, priotitize that spot
-}
-
 function cpuRandomMove(){
+    
     let rand = getRandomInt(0, arrPlayableTiles.length -1); //Choose a random loation based on the avaialble tiles
+    console.log(rand);
+    console.log(arrPlayableTiles);
     arrPlayableTiles[rand].src = activeGamePiece; //get the info from playable tile and apply game piece to it
-    arrPlayableTiles.shift(rand); //remove the tile we just played from the playable array
+    delete arrPlayableTiles[rand]; //remove the tile we just played from the playable array
+    //return an array without the blank indexes
 }
-
 function whoGoesFirst(){
     if(Math.random() < .5){
         activePlayer = "player";
-        PlayerTurn();
     }
     else{
     activePlayer = "cpu";
-    cpuTurn()
     }
 }
-
 function applyGamePiece(){
-        this.src = activeGamePiece; //need to access the tile that queued the event
-        arrPlayableTiles.shift(this);
-        cpuTurn();
-}
+    //alert('selected');
+    this.src = activeGamePiece; //need to access the tile that queued the event
+    //delete arrPlayableTiles(index);  //this throws an error, we need to get the callers info 
+    console.log(this);
+    checkForWinner();
+    
+    activePlayer = 'cpu'
+    cpuTurn();
 
+}
 function clearGameBoard(){
     //loop through each row and clear the board
     arrRowTop.forEach( (element) => element.src = "");
     arrRowMid.forEach( (element) => element.src = "");
     arrRowBot.forEach( (element) => element.src = "");
 }
-
 function checkForWinner(){
     //need condition to test if 3 of the same input is connected
     // Array.allEqual() checks all elements in the array if even
     isWinner = false;
-    const p1Winner = (currentValue) => currentValue = playerGamePiece;
-    const p2Winner = (currentValue) => currentValue = cpuGamePiece;
+    const p1Winner = (currentValue) => currentValue == playerGamePiece;
+    const p2Winner = (currentValue) => currentValue ==  cpuGamePiece;
     
     //Check all rows for same image
     //Check all columns for same image
@@ -155,7 +138,6 @@ function checkForWinner(){
         }
     }))
 }
-
         //*****End Game functions*****
 
 function p1Victory(){
@@ -163,13 +145,11 @@ function p1Victory(){
     //disable inputs
     resetGameBoard();//restart
 }
-
 function p2Victory(){
     alert("CPU Wins"); 
     //disable inputs
     resetGameBoard();
 }
-
 function resetGameBoard(){
     //clear all iamges on board
     clearGameBoard();
@@ -177,10 +157,7 @@ function resetGameBoard(){
     //setup the players
     whoGoesFirst();
 
-    //Lets play Tic Tac Toe
-    PlayGame();
 }
-
         //*****Utility *****/
 
 function getRandomInt(min, max) {
